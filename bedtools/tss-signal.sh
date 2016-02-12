71976315270b95fa52604a09bcb2dd2fe4dd099f
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-02-12 06:37:07 -0700

    plot

---
+++ bedtools/tss-signal.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+tss_bed="tss.bed"
+signal="ctcf.hela.chr22.bg.gz"
+genome="hg19.genome"
+
+bedtools slop -i $tss_bed -b 1000 -g $genome \
+    | bedtools makewindows -b - -n 40 -i winnum \
+    | bedtools sort -i - \
+    | bedtools map -a - -b $signal -c 4 -o mean \
+    | awk '$5 != "."' \
+    | sort -k4n \
+    | bedtools groupby -g 4 -c 5 -o sum \
+    > signal.tab
